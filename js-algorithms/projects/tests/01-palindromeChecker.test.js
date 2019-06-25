6c12e1f8f6a1b1f36854c888b57f285eeb48cd1e
nilsonmolina <nilson13@me.com>
2019-06-25 09:27:02 -0700

    completed first algorithm project and created unit tests

---
+++ js-algorithms/projects/tests/01-palindromeChecker.test.js
@@ -0,0 +1,14 @@
+const { palindrome } = require('../01-palindromeChecker');
+
+describe('Project 1: Palindrome Checker', () => {
+  it('Must pass all of the given examples', () => {
+    expect(palindrome('eye')).toBe(true);
+    expect(palindrome('_eye')).toBe(true);
+    expect(palindrome('race car')).toBe(true);
+    expect(palindrome('not a palindrome')).toBe(false);
+    expect(palindrome('never odd or even')).toBe(true);
+    expect(palindrome('nope')).toBe(false);
+    expect(palindrome('almostomla')).toBe(false);
+    expect(palindrome('My age is 0, 0 si ega ym.')).toBe(true);
+  });
+});
